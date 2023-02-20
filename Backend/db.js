792ce397bfb100ec9780fffc2c191c2e2d1cb8dd
satanikaushal <kaushalsatani@gmail.com>
2023-02-20 23:58:57 +0530

    added atlas

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
-const mongoURI = "mongodb://127.0.0.1:27017/kaushal";
+const mongoURI = "mongodb+srv://satanikaushal:9qLbIIFv1k5WaR0V@cluster0.6ru8n0e.mongodb.net/notebook?retryWrites=true&w=majority";
 
 const connectToMongo = async()=>{
-    mongoose.connect(mongoURI, () => {
-      console.log("Connected to kaushal database");
-   })
+   try {
+      // Connect to the MongoDB cluster
+      mongoose.connect(
+         mongoURI,
+        { useNewUrlParser: true, useUnifiedTopology: true },
+        () => console.log(" Mongoose is connected"),
+      );
+    } catch (e) {
+      console.log("could not connect");
+    }
+    
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+    dbConnection.once("open", () => console.log("Connected to DB!"));
 }
 mongoose.set('strictQuery', false);
 module.exports = connectToMongo;
